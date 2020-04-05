8b47d24035fdbbe64a7a24b56d7c8ad917922532
renjian5203344 <757204311@qq.com>
2020-04-05 18:37:39 +0800

    获取JobDataMap,Map方式一方法2

--- src/main/java/com/yizhan/TestJob.java
+++ src/main/java/com/yizhan/TestJob.java
@@ -17,25 +17,48 @@ public class TestJob implements Job {
          *  具体业务逻辑
          *  System.out.println("Test yizhan!");
          */
+        //第一种方式
         JobKey jobKey =  jobExecutionContext.getJobDetail().getKey();
         System.out.println("My Job name and group are: " + "[" + jobKey.getName() + " ," + jobKey.getGroup() + "]");
-
+        //第一种方式
         TriggerKey triKey = jobExecutionContext.getTrigger().getKey();
 
         System.out.println("My Trigger name and  group are: " + "[" + triKey.getName() + " ," + triKey.getGroup() + "]");
-        JobDataMap dataMap = jobExecutionContext.getJobDetail().getJobDataMap();
-        JobDataMap tdataMap = jobExecutionContext.getTrigger().getJobDataMap();
 
 
-        String jobMsg = dataMap.getString("message");
-        float jobFloatValue = dataMap.getFloat("FloatJobValue");
-        String triggerMsg = tdataMap.getString("message");
-         double triggerDoubleValue = tdataMap.getDouble("DoubleTriggerValue");
+        /****获取JobDataMap，通过Map读取方法一
+         *  JobDataMap dataMap = jobExecutionContext.getJobDetail().getJobDataMap();
+         *  JobDataMap tdataMap = jobExecutionContext.getTrigger().getJobDataMap();
+         *
+         *  String jobMsg = dataMap.getString("message");
+         *  float jobFloatValue = dataMap.getFloat("FloatJobValue");
+         *  String triggerMsg = tdataMap.getString("message");
+         *  double triggerDoubleValue = tdataMap.getDouble("DoubleTriggerValue");
+         *
+         *  System.out.println("jobMsg is:" + jobMsg);
+         *  System.out.println("jobFloatValue is:" + jobFloatValue);
+         *  System.out.println("triggerMsg is:" + triggerMsg);
+         *  System.out.println("triggerDoubleValue is:" +triggerDoubleValue);
+         */
 
-        System.out.println("jobMsg is:" + jobMsg);
-        System.out.println("jobFloatValue is:" + jobFloatValue);
-        System.out.println("triggerMsg is:" + triggerMsg);
-        System.out.println("triggerDoubleValue is:" +triggerDoubleValue);
+
+        /***
+         * 获取JobDataMap，通过Map读取方法二（推荐）
+         *         JobDataMap dataMap = jobExecutionContext.getMergedJobDataMap();
+         *
+         *         String jobMsg = dataMap.getString("message");
+         *         float jobFloatValue = dataMap.getFloat("FloatJobValue");
+         *         String triggerMsg = dataMap.getString("message");
+         *         double triggerDoubleValue = dataMap.getDouble("DoubleTriggerValue");
+         *
+         *
+         *          System.out.println("jobFloatValue is:" + jobFloatValue);
+         *          System.out.println("jobMsg is:" + jobMsg);//jobDetail和trigger key是相同的，trigger为优先，会覆盖掉jobDetail相同key那一个值。
+         *          System.out.println("triggerDoubleValue is:" +triggerDoubleValue);
+         */
+
+
+
 
     }
 }
