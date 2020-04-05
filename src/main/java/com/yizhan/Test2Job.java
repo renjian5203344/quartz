b71441f7e1f35a71131f7653b3867993c505e302
renjian5203344 <757204311@qq.com>
2020-04-05 21:29:02 +0800

    提交Trigger设置开始结束时间代码

--- src/main/java/com/yizhan/Test2Job.java
+++ src/main/java/com/yizhan/Test2Job.java
@@ -36,10 +36,20 @@ public class Test2Job implements Job {
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        //打印当前时间
+        //打印当前执行时间，格式为2020-0101 00：00：00
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("当前时间:"+sdf.format(date));
+        System.out.println("Current Exec Time Is:"+sdf.format(date));
+
+        Trigger currentTrigger = context.getTrigger();
+        System.out.println("Start Time is: " + sdf.format(currentTrigger.getStartTime()));
+        System.out.println("End Time is: " + sdf.format(currentTrigger.getEndTime()));
+        JobKey jobKey = currentTrigger.getJobKey();
+        System.out.println("JobKey info===" + " jobName:" + jobKey.getName()
+                +" ,jobGroup: "+ jobKey.getGroup());
+
+
+
 
 
 
--- src/main/java/com/yizhan/Test2Scheduler.java
+++ src/main/java/com/yizhan/Test2Scheduler.java
@@ -10,21 +10,31 @@ public class Test2Scheduler {
 
     public static void main(String[] args) throws SchedulerException {
 
+
+        //打印当前时间
+        Date date = new Date();
+        Date endDate = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("Current time Is::"+sdf.format(date));
+
         //创建一个JobDetail实例，将该实例与 TestJob绑定
-        JobDetail jobDetail = JobBuilder.newJob(TestJob.class)
+        JobDetail jobDetail = JobBuilder.newJob( Test2Job.class)
                 .withIdentity("myjob","group1")
                 .build();
-/*****
- *      System.out.println(" 【jobDetail】name:" + jobDetail.getKey().getName());
- *      System.out.println(" 【jobDetail】group:" + jobDetail.getKey().getGroup());
- *      System.out.println(" 【jobDetail】group:" + jobDetail.getJobClass().getName());
- *
- */
+
+        //获取当前时间3秒后的时间
+        date.setTime(date.getTime()+3000);
+        //获取当前时间6秒后的时间
+        endDate.setTime(endDate.getTime()+6000);
+
+
 
         //创建Trigger实例(触发job执行) 定义执行时间、会不会重复执行、或执行几次终止
-        //定义该job立即执行、 并且每隔2S执行一次。
+        //定义该job开始时间为当前时间3秒以后，结束时间为当前时间6S以后
         Trigger trigger = TriggerBuilder.newTrigger().withIdentity("myTrigger","group1")
-                .startNow().withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                .startAt(date)
+                .endAt(endDate)
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
                         .withIntervalInSeconds(2).repeatForever()).build();
 
         //创建Scheduler实例（Factory）
@@ -32,10 +42,7 @@ public class Test2Scheduler {
         Scheduler scheduler = sfact.getScheduler();
         scheduler.start();
 
-        //打印当前时间
-        Date date = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("当前时间:"+sdf.format(date));
+
 
         scheduler.scheduleJob(jobDetail,trigger);
 
